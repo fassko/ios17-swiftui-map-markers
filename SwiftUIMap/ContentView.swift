e300bb384196276b56b181d890d03be7151905b6
Kristaps Grinbergs <fassko@gmail.com>
2023-06-28 09:14:28 +0300

    Initial commit

--- SwiftUIMap/ContentView.swift
+++ SwiftUIMap/ContentView.swift
@@ -6,19 +6,45 @@
 //
 
 import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+  static let marker = CLLocationCoordinate2D(latitude: 56.951504503078425, longitude: 24.11330162819283)
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    Map {
+      // the default way
+      // Creates a marker at the given location
+      Marker("Latvian Freedom Monument", coordinate: .marker)
+      
+      // image
+      // Creates a marker at the given location with the provided title and system image to display as the balloon’s icon.
+      //        Marker("Latvian Freedom Monument", image: "monument", coordinate: .marker)
+      
+      // system image with SF symbol a.k.a system image
+      //        Marker("Latvian Freedom Monument", systemImage: "signpost.left.fill", coordinate: .marker)
+      
+      // monogram with text
+      // A string up to three characters to display on the marker’s balloon.
+      // can show more characters
+      //        Marker("Latvian Freedom Monument", monogram: "LV", coordinate: .marker)
+      
+      // custom label
+      //        Marker(coordinate: .marker) {
+      //          Label("Latvian Freedom Monument", image: .monument)
+      //        }
+      //        .tint(.yellow)
+      
+      //          .tint(Color(red: 0.27, green: 0.68, blue: 0.18))
+      //          .tag(1)
+      //        Marker("Airbnb", systemImage:  "eraser", coordinate: .marker)
+      //          .tag(1)
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
